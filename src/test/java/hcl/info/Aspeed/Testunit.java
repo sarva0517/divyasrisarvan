d676535e5fc0a7f236dd88adc2d20a8ed4eeb733
sarva0517 <ndivyasrisarvan@gmail.com>
2022-11-07 13:07:13 +0530

    my new code

---
+++ src/test/java/hcl/info/Aspeed/Testunit.java
@@ -0,0 +1,19 @@
+package hcl.info.Aspeed;
+
+
+
+public class Testunit {
+	public void test1() {
+		System.out.println("test1");
+	}
+	
+	public void test2() {
+		System.out.println("test2");
+	}
+	
+	public void test3() {
+		System.out.println("test 3");
+	
+	}
+
+}
